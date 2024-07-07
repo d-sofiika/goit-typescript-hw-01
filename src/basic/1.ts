20d974845c45ea795a0f71f38f82256dd28fe612
d-sofiika <soniadrozd2709@gmail.com>
2024-07-07 21:38:58 +0200

    done four tasks

---
+++ src/basic/1.ts
@@ -0,0 +1,5 @@
+let Age:number = 50;
+let Username:string = 'Max';
+let Toggle:boolean = true;
+let Empty:null = null;
+const callback = (a:number) :number =>{ return 100 + a };
---
+++ src/basic/3.ts
@@ -0,0 +1,4 @@
+let unionType: string | number;
+
+type LiteralType = "enable" | "disable";
+let value: LiteralType;
